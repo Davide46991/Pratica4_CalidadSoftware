69d3bbb477cfaaf4d9e5c8226350c2a60a471cdb
giuseppearcuti <giuseppearcuti@localhost>
2025-01-18 01:30:26 +0100

    prova

--- src/test/java/com/example/progetto_ic/AppTest.java
+++ src/test/java/com/example/progetto_ic/AppTest.java
@@ -12,11 +12,6 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class AppTest {
-    @Test
-    public void testAdd() {
-        Progetto_IC app = new Progetto_IC();
-        Assert.assertEquals(5, app.add(2, 3)); // Test corretto
-    }
 
     @Test
     public void testSubtract() {
